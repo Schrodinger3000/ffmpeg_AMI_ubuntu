0eb7f1efaa31e4ddc57ee1f605caa4bea2f00b77
Garfilth <andredesbiens@icloud.com>
2018-06-07 07:01:42 -0400

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Update and Upgrade Ubuntu
+Sudo apt-get update && upgrade -y ;\
+
+#install FFmpeg
+sudo add-add-repository ppa:djcj/hybrid ;\
+sudo apt-get update ;\
+sudo apt-get install ffmpeg -y ;\
+
+#install aws CLI
+sudo apt install awscli -y ;\
+
+#Create Script Directory and permission
+sudo mkdir /home/ubuntu/MyScript ;\
+sudo mkdir /home/ubuntu/Encoded ;\
+sudo mkdir /home/ubuntu/Proxies ;\
+sudo chmod 640 -R ~/MyScript ;\
+sudo chmod 640 -R ~/Encoded ;\
+sudo chmod 640 -R ~/Proxies ;\
+
+#Transfer srpits from s3
+aws s3 mv s3://erdna-test/scripts/ ~/MyScript/ --recursive ;\
+
+#complete recursive permission
+sudo chmod 777 -R ~/MyScript ;\
+
+done
